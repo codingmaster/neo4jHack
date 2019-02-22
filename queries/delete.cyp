4d9d337af799a02eb7b2198aea5345fa46de726d
mwolowyk <michael.wolowyk@springernature.com>
2019-02-22 11:52:32 +0100

    import springer books

---
+++ queries/delete.cyp
@@ -0,0 +1,1 @@
+MATCH (n:Book) DETACH DELETE n
---
+++ src/main/java/com/spring/hack/marvel/comicissue/ComicIssueRepo.java
@@ -0,0 +1,16 @@
+package com.spring.hack.marvel.comicissue;
+
+import org.springframework.data.neo4j.annotation.Query;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+
+public interface ComicIssueRepo extends Neo4jRepository<ComicIssue, Long> {
+    ComicIssue findByName(@Param("name") String name);
+
+    Iterable<ComicIssue> findByNameLike(@Param("name") String name);
+
+    @Query("MATCH (i:ComicIssue)-[r]-(n) RETURN i,r,n LIMIT {limit}")
+    Collection<ComicIssue> graph(@Param("limit") int limit);
+}
